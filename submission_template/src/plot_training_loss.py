2c150ca4cbee1db01f0e4623c3751017426f9327
juusano13o <hoangtunguyen2002@gmail.com>
2026-08-10 14:05:31 +0200

    fix graph's visual

--- submission_template/src/plot_training_loss.py
+++ submission_template/src/plot_training_loss.py
@@ -1,6 +1,7 @@
 import re
 
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 
 LOG_FILE = "submission_template/src/log.txt"
 OUTPUT_FILE = ""
@@ -8,44 +9,58 @@ OUTPUT_FILE = ""
 
 def read_training_log(log_file):
     epochs = []
-    losses = []
+    train_mse_values = []
+    val_mae_values = []
+    val_rmse_values = []
 
     with open(log_file, "r") as f:
         for line in f:
             match = re.search(
-                r"Epoch \[(\d+(?:\.\d+)?)\] Train Loss: ([\d.eE+-]+)", line
+                r"epoch=(\d+)\s+"
+                r"train_mse=([\d.eE+-]+)\s+"
+                r"val_mae=([\d.eE+-]+)\s+"
+                r"val_rmse=([\d.eE+-]+)",
+                line,
             )
-
             if match:
-                epoch_progress = float(match.group(1))
-                loss = float(match.group(2))
+                epoch = int(match.group(1))
+                train_mse = float(match.group(2))
+                val_mae = float(match.group(3))
+                val_rmse = float(match.group(4))
 
-                # Convert [0.04, 0.08, ...] back to epoch number
-                # if the log was created with 25 epochs.
-                epochs.append(epoch_progress)
-                losses.append(loss)
+                epochs.append(epoch)
+                train_mse_values.append(train_mse)
+                val_mae_values.append(val_mae)
+                val_rmse_values.append(val_rmse)
 
-    return epochs, losses
+    return epochs, train_mse_values, val_mae_values, val_rmse_values
 
 
 def main():
-    epochs, losses = read_training_log(LOG_FILE)
+    epochs, train_mse_values, val_mae_values, val_rmse_values = read_training_log(
+        LOG_FILE
+    )
 
-    if not losses:
-        print("No training loss found in the log file.")
-        return
+    # if not losses:
+    # print("No training loss found in the log file.")
+    # return
 
-    plt.figure(figsize=(8, 5))
+    plt.figure(figsize=(100, 60))
+    plt.plot(epochs, train_mse_values, marker="o", label="Train MSE")
+    plt.plot(epochs, val_mae_values, marker="o", label="Validation MAE")
+    plt.plot(epochs, val_rmse_values, marker="o", label="Validation RMSE")
 
-    plt.plot(epochs, losses, marker="o")
+    plt.xlabel("Epoch", fontsize=32)
+    plt.ylabel("", fontsize=32)
+    plt.gca().yaxis.set_major_locator(MultipleLocator(0.5))
 
-    plt.xlabel("Epoch")
-    plt.ylabel("Train Huber Loss")
-    plt.title("Training Loss")
+    plt.tick_params(axis="both", labelsize=16)
 
+    plt.title("PatchTST Training and Validation Metrics", fontsize=32)
+
+    plt.legend(fontsize=18)
     plt.grid(True, alpha=0.3)
-    plt.tight_layout()
-
+    plt.xticks(epochs)
     # use when want to save the graph
     # plt.savefig(OUTPUT_FILE, dpi=150)
     plt.show()
